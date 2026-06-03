7803530a919029a82554337aae3bc6ec18ba8bdc
celine265 <celine265@users.noreply.github.com>
2026-06-03 07:03:50 +0000

    deploy: c7ff4124a1403898aac1d74d9a0f85812be41d08

--- main.dart.js
+++ main.dart.js
@@ -27272,7 +27272,7 @@ $S:2}
 A.arr.prototype={
 $1(a){var s=A.d1().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/4c525dac5ebe5971c5708ef73558ed8edcf4a362/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/c416acfeb8126e097f758c664aaa3da929e27da0/":s)+a},
 $S:117}
 A.Lq.prototype={
 gA(a){var s=this.a
